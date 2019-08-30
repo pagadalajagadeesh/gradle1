c521aed63099908053fb09432f25e5d8d7c255d8
Jagadeesh Pagadala <Jagadeesh.Pagadala@jispl.com>
2019-08-30 12:28:09 +0530

    first commit

---
+++ src/main/java/com/jenkins/demoZTest/DemoZTestApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.demoZTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoZTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoZTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/demoZTest/DemoZTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.demoZTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoZTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
